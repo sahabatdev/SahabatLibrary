4986274eb3fc8963bcf94d37c2361c583be819b0
Maulana Rahmatullah <rahmatullahalan@gmail.com>
2019-07-02 07:22:04 +0700

    Update Konversi Rupiah

--- sahabatlibrary/src/main/java/site/sahabatdeveloper/sahabatlibrary/helper/SahabatHelper.java
+++ sahabatlibrary/src/main/java/site/sahabatdeveloper/sahabatlibrary/helper/SahabatHelper.java
@@ -4,20 +4,30 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
-public class SahabatHelper<T> {
-    public T convertJsonToModel(String response) {
-        Gson gson = new Gson();
-        Type type = new TypeToken<T>() {
-        }.getType();
-        T obj = gson.fromJson(response, type);
-        if (obj != null) {
-            return obj;
-        } else return null;
+public class SahabatHelper {
+    static DecimalFormat df= new DecimalFormat("#,##0");
+    public static String convertRupiah(int nominal){
+        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ITALY));
+        return df.format(Double.valueOf(nominal));
     }
 
-    public String convertModelToJson(T response) {
-        Gson gson = new Gson();
-        return gson.toJson(response);
+    public static String convertRupiah(String nominal){
+        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ITALY));
+        return df.format(Double.valueOf(nominal));
+    }
+
+    public static String convertRupiah(double nominal){
+        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ITALY));
+        return df.format(nominal);
+    }
+
+    public static String convertRupiahDecimal(double nominal){
+        DecimalFormat dfd= new DecimalFormat("#,##0.00");
+        dfd.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.ITALY));
+        return dfd.format(nominal);
     }
 }
